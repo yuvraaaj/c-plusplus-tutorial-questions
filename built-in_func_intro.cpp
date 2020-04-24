5d915203416e3b1328069c189362a84b8932bbfe
Yuvraj Manral <42829486+yuvrajmanral@users.noreply.github.com>
2020-04-24 19:19:39 +0530

    Add files via upload
    Q) Inside of main(), call rand() with the modulo operator to generate a random number between 0 and your favorite number. For example, rand() % 29 would output a random number between 0 and 28.
    
    Assign the resulting value to a new int variable called the_amazing_random_number.
    
    Print the_amazing_random_number to the terminal.

---
+++ built-in_func_intro.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+  // This seeds the random number generator:
+  srand (time(NULL));
+  // Use rand() below to initialize the_amazing_random_number
+  int the_amazing_random_number = rand() % 29;
+  std::cout << the_amazing_random_number;
+}
